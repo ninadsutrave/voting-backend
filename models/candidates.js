1ed9d3f346151eb1eaf25ed3c1cb7b383fe29db0
Ninad Sutrave <ninadsutrave@gmail.com>
2023-06-12 17:23:16 +0530

    addind candidate schema

---
+++ models/candidates.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose"
+
+const Schema = mongoose.Schema
+
+const candidateSchema = new Schema({
+    name: {
+      type: String,
+      required: true
+    },
+    votes: {
+      type: Number,
+      default: 0
+    },
+    post: {
+      type: String,
+      ref: 'PostNames',
+      required: true
+    }
+});
+
+const Candidates = mongoose.model('candidate', candidateSchema)
+
+export default Candidates
+
